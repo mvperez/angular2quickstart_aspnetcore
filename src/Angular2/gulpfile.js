f99b626b43fa561a4de7743fd8830867d729b577
Marvin Perez <marvin.perez@coherentasia.com>
2016-05-11 16:46:57 +0800

    initial commit

---
+++ src/Angular2/gulpfile.js
@@ -0,0 +1,33 @@
+﻿var ts = require('gulp-typescript');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+var destPath = './wwwroot/libs/';
+
+// Delete the libs directory
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+//Moves Angular 2
+gulp.task("angular", () => {
+    gulp.src([
+            'es6-shim/**',
+            'systemjs/dist/**',
+            'reflect-metadata/**',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'angular2-in-memory-web-api/**',
+            'bootstrap/**',
+            'jquery/**',
+            'es6-promise/**'
+    ],
+    {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest("./wwwroot/libs"));
+});
+
+gulp.task('default', ['angular']);
